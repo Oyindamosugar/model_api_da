745e8ae7a8f94cfadf6f8f641aa1e6cff8fc49e3
Oyindamosugar <158524683+Oyindamosugar@users.noreply.github.com>
2024-05-09 16:29:49 +0100

    Update classify_requirements_chat_ai_relationship.py

--- classify_requirements_chat_ai_relationship.py
+++ classify_requirements_chat_ai_relationship.py
@@ -14,6 +14,7 @@ from driver.neo4j import Neo4jDatabase
 from llm.openai import OpenAIChat
 from summarize_cypher_result import SummarizeCypherResult
 from text2cypher import Text2Cypher
+import boto3
 
 # Maximum number of records used in the context
 HARD_LIMIT_CONTEXT_RECORDS = 10
@@ -43,8 +44,43 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+def list_all_files(bucket_name):
+    """
+    List all files in an S3 bucket.
+
+    :param bucket_name: str - Name of the S3 bucket
+    :return: list - A list of file keys in the bucket
+    """
+    # Create a session using environment variables for AWS credentials
+    session = boto3.Session(
+        aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID','AKIA3FLD43Z6AZA2XKWT'),
+        aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY', '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'),
+        region_name=os.getenv('AWS_DEFAULT_REGION', 'eu-north-1')  # Default to us-east-1 if region not set
+    )
+
+    s3 = session.client('s3')
+
+    # Initialize list to hold file keys
+    file_keys = []
+
+    # Paginate through results to handle large buckets
+    paginator = s3.get_paginator('list_objects_v2')
+    for page in paginator.paginate(Bucket=bucket_name):
+        # Check if 'Contents' key is present in the response dictionary
+        if 'Contents' in page:
+            for obj in page['Contents']:
+                file_keys.append(obj['Key'])
+
+    return file_keys
+
+# Usage Example
+bucket = 'dissertationartefact'
+# Replace with your S3 bucket name
+#files = list_all_files(bucket)
+#print("Files in S3 bucket:", files)
+
 # Load the Saved Model
-model_path = 'model'  # Update this to the path of your saved model
+model_path = 'dissertationartefact'  # Update this to the path of your saved model
 loaded_model = tf.keras.models.load_model(model_path)
 tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 
